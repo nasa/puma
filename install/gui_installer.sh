543702fa149b11c4055d808046b351727d5ff1c0
Federico Semeraro <federicosemeraro94@gmail.com>
2021-07-08 12:38:06 -0700

    Fixed libGL problem, but still undef ref for GUI on linux

--- install/gui_installer.sh
+++ install/gui_installer.sh
@@ -7,10 +7,14 @@ conda activate puma
 
 # this is to fix a libGL path problem on linux
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	FILE="$CONDA_PREFIX"/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64/libGL.so.1
+	VAR=$(conda list mesa-libgl-cos6-x86_64)
+	if [[ $VAR != *"mesa-libgl-cos6-x86_64"* ]]; then
+	    conda install -y mesa-libgl-cos6-x86_64==11.0.7
+	fi
+	FILE="$CONDA_PREFIX"/x86_64-conda-linux-gnu/sysroot/usr/lib64/libGL.so.1
 	if [ -f "$FILE" ]; then
-	    cp -r "$CONDA_PREFIX"/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64 "$CONDA_PREFIX"/x86_64-conda-linux-gnu/sysroot/usr
-	    cp "$CONDA_PREFIX"/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64/libGL.so.1 "$CONDA_PREFIX"/x86_64-conda-linux-gnu/sysroot/usr/lib64/libGL.so
+	    cp -r "$CONDA_PREFIX"/x86_64-conda-linux-gnu/sysroot/usr/lib64 "$CONDA_PREFIX"/x86_64-conda_cos6-linux-gnu/sysroot/usr
+	    cp "$CONDA_PREFIX"/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64/libGL.so.1 "$CONDA_PREFIX"/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64/libGL.so
 	fi
 fi
 
