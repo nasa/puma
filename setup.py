3704c784f037000d5ffebdd8ff83fab48ee76f24
fsemerar <federicosemeraro94@gmail.com>
2024-03-13 15:21:52 -0700

    Fixed setup.py pin for scipy

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     install_requires=[  # TexGen also required, but it can be installed as add-on
         "numpy",
         "scikit-image >=0.17",  # in order to have marching_cubes in the API
-        "scipy =1.11",  # to avoid change in API
+        "scipy ~=1.11",  # to avoid change in API
         "matplotlib",
         "pyvista",
     ],
